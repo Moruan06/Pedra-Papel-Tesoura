4d1817f3a673d25c54d192d7b18d3561df210ea0
Moruan06 <moruan.mattar@gmail.com>
2025-07-05 14:29:22 -0300

    Add function 'playRound' if the computer choice is 'pedra'

--- script.js
+++ script.js
@@ -2,26 +2,43 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice(){
-    let choice = Math.floor(Math.random() * 3);
-    switch(choice){
+    let computerChoice = Math.floor(Math.random() * 3);
+    switch(computerChoice){
         case(0):
-            choice = "pedra";
+            computerChoice = "pedra";
             break;
         case(1):
-            choice = "papel";
+            computerChoice = "papel";
             break;
         case(2):
-            choice = "tesoura";
+            computerChoice = "tesoura";
             break;
     }
-    return choice;
+    return computerChoice;
 }
-
-console.log(getComputerChoice())
 
 function getHumanChoice(){
     let humanChoice = prompt("Escolha sua jogada:");
+    humanChoice = humanChoice.toLowerCase();
     return humanChoice;
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice){
+    if(computerChoice === "pedra"){
+        switch(humanChoice){
+            case("papel"):
+                console.log("Papel ganha de pedra, você ganhou!");
+                humanScore += 1;
+                break;
+            case("tesoura"):
+                console.log("Tesoura perde de pedra, você perdeu ;-;");
+                computerScore +=1;
+                break;
+            case("pedra"):
+                console.log("Ambos jogaram pedra, vocês empataram!");
+                break;
+            default:
+                console.log("Jogada não definida");
+        };
+    };
+}
