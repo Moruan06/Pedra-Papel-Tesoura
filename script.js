420885762bd353c41c80ed88bd7579dc4f2c42eb
Moruan06 <moruan.mattar@gmail.com>
2025-07-05 14:12:00 -0300

    Add the function to get the user choice

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
     switch(choice){
         case(0):
-            choice = "Rock";
+            choice = "pedra";
             break;
         case(1):
-            choice = "Paper";
+            choice = "papel";
             break;
         case(2):
-            choice = "Scissor";
+            choice = "tesoura";
             break;
     }
-    return choice
+    return choice;
 }
 
 console.log(getComputerChoice())
+
+function getHumanChoice(){
+    let humanChoice = prompt("Escolha sua jogada:");
+    return humanChoice;
+}
+
+console.log(getHumanChoice());
